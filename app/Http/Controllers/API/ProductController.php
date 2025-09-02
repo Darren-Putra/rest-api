c6a0569299d13daf26a9f510cab9464f0e82d4c8
Darren-Putra <darren.misyantono05@gmail.com>
2025-09-02 10:05:05 +0800

    update api

---
+++ app/Http/Controllers/API/ProductController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductCollection;
+use App\Models\Product;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index(){
+        $product = Product::latest()->paginate(10);
+        return response()->json(new ProductCollection($product), Response::HTTP_OK);
+
+        // return new ProductCollection($product);
+    }
+}
