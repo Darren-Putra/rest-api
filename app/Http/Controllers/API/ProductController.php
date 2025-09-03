43d0d101469ee0cf2f476f87bb13578fe5aaaf64
Darren-Putra <darren.misyantono05@gmail.com>
2025-09-03 09:43:04 +0800

    Add store method to ProductController for creating products

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\ProductCollection;
+use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
@@ -15,5 +17,23 @@ class ProductController extends Controller
         return response()->json(new ProductCollection($product), Response::HTTP_OK);
 
         // return new ProductCollection($product);
+    }
+
+    public function store(ProductRequest $request){
+        $product = Product::create($request->validated());
+        return response()->json([
+            'status' => true,
+            'message' => 'Product berhasil ditambahkan',
+            'data' => new ProductResource($product),
+        ], Response::HTTP_CREATED);
+    }
+    public function show(){
+
+    }
+    public function update(){
+
+    }
+    public function destroy(){
+
     }
 }
