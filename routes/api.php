9ccc64aba7aa87ea7decd209fbc044009e0ed87e
Darren-Putra <darren.misyantono05@gmail.com>
2025-09-19 09:12:20 +0800

    lagi nambahin role

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,24 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\ProductController;
-use App\Http\Resources\ProductCollection;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+Route::prefix('auth')->name('auth.')->group(function () {
+    Route::post('register', [AuthController::class, 'register'])->name('register');
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 
+    Route::middleware('jwt')->group(function () {
+        Route::get('me', [AuthController::class, 'me'])->name('me');
+        Route::post('refresh', [AuthController::class, 'refresh'])->name('refresh');
 
-Route::apiResource('products', ProductController::class);
+        // 🔹 Tambah route change password
+        Route::post('change-password', [AuthController::class, 'changePassword'])->name('change-password');
+    });
+});
+
+// 🔹 Resource Product tetap dilindungi jwt
+Route::middleware('jwt')->group(function () {
+    Route::apiResource('products', ProductController::class);
+});
